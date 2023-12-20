da483d8b7b175c69dc34a0b822723f1019197c34
Pierre Jeunesse <pierre.jeunesse@institutoptique.fr>
2023-12-20 16:21:06 +0100

    Delete Arduino_telemetrix.py

--- src/pymodaq_plugins_teaching/hardware/Arduino_telemetrix.py
+++
@@ -1,9 +0,0 @@
-
-
-class Arduino_telemetrix:
-    def __init__(self, com_port=None, arduino_instance_id=1,
-                 arduino_wait=4, sleep_tune=0.000001,
-                 shutdown_on_exception=True):
-
-
-
